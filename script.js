14afbcea166385eea6c604b62d73a4bc10d9b852
swarajmutalik <swarajmutalik@gmail.com>
2025-07-09 16:07:33 +0530

    Initial Condition Setup

--- script.js
+++ script.js
@@ -1,1 +1,11 @@
-'use strict';
+"use strict";
+
+// Selecting elements
+const score0element = document.querySelector("#score--0");
+const score1element = document.getElementById("score--1");
+const diceelement = document.querySelector(".dice");
+
+// Starting Conditions
+score0element.textContent = 0;
+score1element.textContent = 0;
+diceelement.classList.add("hidden");
