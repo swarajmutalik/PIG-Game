13e8df34826b74970e3582448bc5a4f3f71a78b9
swarajmutalik <swarajmutalik@gmail.com>
2025-07-11 11:24:26 +0530

    Resetting all the values back to normal

--- script.js
+++ script.js
@@ -13,35 +13,80 @@ const btnNew = document.querySelector(".btn--new");
 const btnRoll = document.querySelector(".btn--roll");
 const btnHold = document.querySelector(".btn--hold");
 
-// Starting Conditions
-score0element.textContent = 0;
-score1element.textContent = 0;
-diceelement.classList.add("hidden");
+let scores, currentScore, activePlayer, playing;
 
-const scores = [0, 0];
-let currentScore = 0;
-let activePlayer = 0;
+// Starting Conditions
+
+const init = function () {
+  scores = [0, 0];
+  currentScore = 0;
+  activePlayer = 0;
+  playing = true;
+
+  score0element.textContent = 0;
+  score1element.textContent = 0;
+  current0element.textContent = 0;
+  current1element.textContent = 0;
+
+  player0element.classList.remove("player--winner");
+  player1element.classList.remove("player--winner");
+  player0element.classList.add("player--active");
+  player1element.classList.remove("player--active");
+  diceelement.classList.add("hidden");
+};
+
+init();
+
+const switchPlayer = function () {
+  document.getElementById(`current--${activePlayer}`).textContent = 0;
+  currentScore = 0;
+  activePlayer = activePlayer === 0 ? 1 : 0;
+  player0element.classList.toggle("player--active");
+  player1element.classList.toggle("player--active");
+};
 
 // Rolling Dice Functionality
 btnRoll.addEventListener("click", function () {
-  // Generating a random dice role
-  const dice = Math.trunc(Math.random() * 6) + 1;
-  // Display Dice
-  diceelement.classList.remove("hidden");
-  diceelement.src = `dice-${dice}.png`;
+  if (playing) {
+    // Generating a random dice role
+    const dice = Math.trunc(Math.random() * 6) + 1;
+    // Display Dice
+    diceelement.classList.remove("hidden");
+    diceelement.src = `dice-${dice}.png`;
 
-  // Check for Rolled 1: if true, switch to next player
-  if (dice !== 1) {
-    // Add dice to the current score
-    currentScore += dice;
-    document.getElementById(`current--${activePlayer}`).textContent =
-      currentScore;
-  } else {
-    // Switch to next player
-    document.getElementById(`current--${activePlayer}`).textContent = 0;
-    currentScore = 0;
-    activePlayer = activePlayer === 0 ? 1 : 0;
-    player0element.classList.toggle("player--active");
-    player1element.classList.toggle("player--active");
+    // Check for Rolled 1: if true, switch to next player
+    if (dice !== 1) {
+      // Add dice to the current score
+      currentScore += dice;
+      document.getElementById(`current--${activePlayer}`).textContent =
+        currentScore;
+    } else {
+      // Switch to next player
+      switchPlayer();
+    }
   }
 });
+
+btnHold.addEventListener("click", function () {
+  if (playing) {
+    // Add current score to the active player's score
+    scores[activePlayer] += currentScore;
+    document.getElementById(`score--${activePlayer}`).textContent =
+      scores[activePlayer];
+
+    if (scores[activePlayer] >= 100) {
+      playing = false;
+      diceelement.classList.add("hidden");
+      document
+        .querySelector(`.player--${activePlayer}`)
+        .classList.add("player--winner");
+      document
+        .querySelector(`.player--${activePlayer}`)
+        .classList.remove("player--active");
+    } else {
+      switchPlayer();
+    }
+  }
+});
+
+btnNew.addEventListener("click", init);
